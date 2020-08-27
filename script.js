50b7cb3f3d6d0e9c5331657b32bbfcb66ac0d3c1
Arayik19 <ahambardzumyan19@gmail.com>
2020-08-27 13:48:19 +0400

    Code optimized,new validation added.

--- script.js
+++ script.js
@@ -1,87 +1,90 @@
 //======= firebase
-const database = firebase.database(); 
+const database = firebase.database();
 const ref = database.ref('users');
-
-//======= input variables
-const nameInput = document.querySelector('#name');
-const emailInput = document.querySelector('#email');
-const passwordInput = document.querySelector('#pass');
-const confirmPasswordInput = document.querySelector('#repass');
-const createdPasswordInput = document.querySelector('#createdPass');
-const createdLoginInput = document.querySelector('#createdLogin');
-
 //======== validation variables
 const errorElement = document.getElementById('error');
 const emailValidationVar = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 const form = document.getElementById("registerForm");
-
 const dataObject = {
     name: "",
     email: "",
     password: "",
-    confirmPassword: "",
 };
+
+class Login {
+    constructor(name, pass) {
+        this.name = name;
+        this.pass = pass;
+    }
+}
+class Register extends Login {
+    constructor(name, pass, email, cpass) {
+        super(name, pass);
+        this.email = email;
+        this.cpass = cpass;
+    }
+}
+
+const login = new Login(document.querySelector('#name'),
+    document.querySelector('#pass'));
+
+const register = new Register(document.querySelector('#name'),
+    document.querySelector('#pass'),
+    document.querySelector('#email'),
+    document.querySelector('#repass'));
 //==============================
 
-
-document.getElementById("loginForm").addEventListener("submit", (e) => {
-    e.preventDefault();
-})
-
-function formValidate(){
+function formValidate(data) {
     let messages = []
-    if (nameInput.value === '' || nameInput.value == null) {
+    if (register.name.value === '' || register.name.value == null) {
         messages.push('Name is required')
     }
-    if (passwordInput.value.length <= 6) {
+    if (register.pass.value.length <= 6) {
         messages.push('Password must be longer than 6 characters')
     }
-    if (passwordInput.value.length >= 20) {
+    if (register.pass.value.length >= 20) {
         messages.push('Password must be less than 20 characters')
     }
-    if(emailValidationVar.test(emailInput.value)==false){
+    if (emailValidationVar.test(register.email.value) == false) {
         messages.push('Incorrect email format')
     }
-    if (confirmPasswordInput.value !== passwordInput.value) {
+    const users = Object.values(Object.assign([], data.val()));
+    console.log(users);
+    const index = users.findIndex(x => (x.name === register.name.value || x.email === register.email.value))
+    if (index !== -1) {
+        messages.push('Email or Username already exists!')
+    }
+    if (register.cpass.value !== register.pass.value) {
         messages.push('Passwords must match!')
-    }    
+    }
     if (messages.length > 0) {
         errorElement.innerText = messages.join(', ')
         errorElement.style.backgroundColor = "#fb8989";
-        return false;
-    }else{
+    } else {
         createAccount()
-        return true;
+        location.replace("login.html")
     }
 }
 
 function createAccount() {
-    dataObject.name = nameInput.value;
-    dataObject.email = emailInput.value;
-    dataObject.password = passwordInput.value;
-    dataObject.confirmPassword = confirmPasswordInput.value;
+    dataObject.name = register.name.value;
+    dataObject.email = register.email.value;
+    dataObject.password = register.pass.value;
     ref.push(dataObject);
     alert("Congratulations, you have created an account!");
 }
 
 function logIn(data) {
-    let users = data.val();
-    let keys = Object.keys(users);
-    for (let i = 0; i < keys.length; i++) {
-        let k = keys[i];
-        let name = users[k].name;
-        let password = users[k].password;        
-        let email = users[k].email;
-        if (createdPasswordInput.value === password
-            && (createdLoginInput.value === name || createdLoginInput.value === email)) {
-            alert("You are now Signed In!")
-        }
-        else {
-            alert("Wrong Password or Login credentials!")
-        }
+    const users = Object.values(data.val());
+    const index = users.findIndex(x => (x.name === login.name.value || x.email === login.name.value)
+        && x.password === login.pass.value);
+    if (index !== -1) {
+        alert("You are now Signed In!")
+    }
+    else {
+        alert("Wrong Password or Login credentials!")
     }
 }
-
 function errData(err) {
     console.log("Error!");
     console.log(err)
